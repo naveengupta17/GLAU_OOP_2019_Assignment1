ef951ecff80dc7d31d960a06f3b8a04432269496
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2019-08-18 10:57:42 +0530

    Gravity.java declaration of gravity constant

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -1,20 +1,17 @@
 /*
- * @author Divyansh Bhardwaj
+ * @author Naveen gupta
  * @version 1.0
  * */
 package main;
 
-// import java.util.Scanner;
-// uncomment the above line to use input in your program.
+import java.util.Scanner;
 
 public class Gravity {
-	// TODO 1: Create the main method
 	public static void main(String[] args) {
-		/* TODO 2:
-		 *   a. Declare all your necessary variables inside main method.
-		 *   b. Declare a constant for 'gravity', assume the value to be 9.8
-		 *   c. Calculate the result and print it to the screen.
-		 * */
+		Scanner scanner = new Scanner(System.in);
+		double g = 9.8;
+
+
 	}
-	// P.S: Refer to the README.md file for the problem statement, input & output.
 }
+
