b085fa5602744da345d5b967079479cd24573a5d
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2019-08-22 21:43:03 +0530

    adding output statement

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -14,7 +14,7 @@ public class Gravity {
         double speed = (g * time);
         double distance = (g * time * time) / 2;
         System.out.println("The speed of the object at " + time + " seconds after its release is " + speed + " and the distance the object has travelled in the " + time + " seconds after the release is " + distance);
-
+        scanner.close();
 
 
 
